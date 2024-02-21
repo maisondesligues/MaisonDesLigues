03b193a51d4ad49148188fccc2f3c79d26a59463
Lemufi <Realphenix220@gmail.com>
2024-02-21 16:57:11 +0100

    Nuite add

---
+++ src/Entity/Nuite.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NuiteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NuiteRepository::class)]
+class Nuite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $datenuitee = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDatenuitee(): ?\DateTimeInterface
+    {
+        return $this->datenuitee;
+    }
+
+    public function setDatenuitee(\DateTimeInterface $datenuitee): static
+    {
+        $this->datenuitee = $datenuitee;
+
+        return $this;
+    }
+}
