4ceb1653ae91b880c6bf2a14f966013b58b6bc90
yousra EL YEBDRI <ely.yousra090705@gmail.com>
2024-02-22 12:59:11 +0100

    accueil

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,20 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\AtelierRepository; 
+use App\Repository\AtelierRepository;
 use App\Repository\HotelRepository;
+use App\Repository\CategorieChambreRepository;
+
 
 class BaseController extends AbstractController
 {
      #[Route('/accueil', name: 'app_base')]
-    public function index(AtelierRepository $atelierRepository, HotelRepository $hotelRepository): Response
+     public function index(AtelierRepository $atelierRepository, HotelRepository $hotelRepository, CategorieChambreRepository $categorieChambreRepository): Response
     {
         $ateliers = $atelierRepository->findAll();
         $hotels = $hotelRepository->findAll();
+        $categoriesChambres = $categorieChambreRepository->findAll();
 
         return $this->render('Accueil.html.twig', [
             'ateliers' => $ateliers,
             'hotels' => $hotels,
+            'categoriesChambres' => $categoriesChambres,
         ]);
     }
     
