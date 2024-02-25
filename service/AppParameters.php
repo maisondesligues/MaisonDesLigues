213dd50cd5f54d93c74e1021d467a3a206b68f93
Lemufi <59792742+Lemufi@users.noreply.github.com>
2024-02-25 14:41:18 +0100

    Paramètres crées

---
+++ service/AppParameters.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\service;
+
+class AppParameters
+{
+    private $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
+
+    public function getIbisHotelSinglePrix()
+    {
+        return $this->params->get('ibis_hotel_single_prix');
+    }
+
+    public function getIbisHotelDoublePrix()
+    {
+        return $this->params->get('ibis_hotel_double_prix');
+    }
+    
+    public function getBudgetHotelSinglePrix()
+    {
+        return $this->params->get('budget_hotel_single_prix');
+    }
+    
+    public function getBudgetHotelDoublePrix()
+    {
+        return $this->params->get('budget_hotel_double_prix');
+    }
+    
+    public function getRepasAccompagnantPrix()
+    {
+        return $this->params->get('repas_accompagnant_prix');
+    }
+    
+    public function getInscriptionCongresPrix()
+    {
+        return $this->params->get('inscription_congres_prix');
+    }
+}
